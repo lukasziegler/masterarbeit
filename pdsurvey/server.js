7f144c61b64cd27bb97695533a49a3067fb770e4
lukasziegler <lukas@lukasziegler.com>
2014-11-26 10:59:34 +0100

    added error handling inspired by https://github.com/cwbuecheler/node-tutorial-2-restful-app

--- pdsurvey/server.js
+++ pdsurvey/server.js
@@ -28,6 +28,37 @@ require('./app/routes')(app);
 app.get('/ping', controllers.index)
 
 
+/// catch 404 and forwarding to error handler
+if (app.get('env') === 'development') {
+	app.use(function(req, res, next) {
+	    var err = new Error('Not Found');
+	    err.status = 404;
+	    next(err);
+	});
+}
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 
 /** Launch server **/
 var server = app.listen(app.get('port'), function () {
