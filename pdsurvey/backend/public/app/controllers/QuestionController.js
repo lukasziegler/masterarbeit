ac9cf9bf7d1234c9a6c8bce3d0eff4e49bf10594
lukasziegler <lukas@lukasziegler.com>
2014-12-19 14:33:41 +0100

    cleaned up Angular controllers one JS file per Schema

---
+++ pdsurvey/backend/public/app/controllers/QuestionController.js
@@ -0,0 +1,73 @@
+var app = angular.module("pdsurvey");
+
+
+/** LIST **/
+
+app.controller("QuestionListController", function($scope, $http) {
+	
+	$http.get("http://localhost:3000/api/questions").success(function(response) {
+		$scope.questions = response;
+	}).error(function(err) {
+		$scope.error = err;
+	});
+
+	$scope.deleteQuestion = function(question) {
+		$http.delete("http://localhost:3000/api/questions/" + question._id)
+			.success(function(response) {
+				var index = $scope.questions.indexOf(question)
+				$scope.questions.splice(index, 1);     
+			});
+	};
+
+});
+
+
+
+/** CREATE **/
+
+app.controller("QuestionCreateController", function($scope, $http, $location) {
+	$scope.question  = {};
+
+	// Load data
+	$http.get("http://localhost:3000/api/questionTypes").success(function(response) {
+		$scope.questionTypes = response;
+	}).error(function(err) {
+		$scope.error = err;
+	});
+
+	// Save data
+	$scope.createQuestion = function() {
+		$http.post("http://localhost:3000/api/questions", $scope.question)
+			.success(function(response) {
+				$location.url("/questions");
+			});
+	}
+});
+
+
+
+/** EDIT **/
+
+app.controller("QuestionEditController", function($scope, $http, $location, $routeParams) {
+	$scope.question  = {};
+	var id = $routeParams.id;
+
+	// Load data
+	$http.get("http://localhost:3000/api/questions/" + id).success(function(response) {
+		$scope.question = response;
+	});
+
+	$http.get("http://localhost:3000/api/questionTypes").success(function(response) {
+		$scope.questionTypes = response;
+	}).error(function(err) {
+		$scope.error = err;
+	});
+
+	// Save data
+	$scope.saveQuestion = function() {
+		$http.put("http://localhost:3000/api/questions/" + $scope.question._id, $scope.question)
+			.success(function(response) {
+				$location.url("/questions");
+			});
+	};
+});
