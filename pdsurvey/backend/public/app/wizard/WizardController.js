a1880bdf3d812ced8395f69379371350f3700888
lukasziegler <lukas@lukasziegler.com>
2015-02-09 17:02:47 +0100

    added new Panels to Wizard

--- pdsurvey/backend/public/app/wizard/WizardController.js
+++ pdsurvey/backend/public/app/wizard/WizardController.js
@@ -13,6 +13,7 @@ var wizard = angular.module('pdWizard', [])
 					    '<div class="col-lg-12" style="padding-bottom: 1.5em;">'+
 					     '   <a href="" class="btn btn-default" ng-click="prevPill()" ng-hide="prevPillShow()">Previous Step</a>'+
 						 '   <a href="" class="btn btn-success pull-right" ng-click="nextPill()" ng-hide="nextPillShow()">Next Step</a>'+
+						 '   <a href="" class="btn btn-warning pull-right" ng-click="nextPill()" ng-show="nextPillShow()">Launch Campaign</a>'+
 					    '</div>'+
 					'</div>',
 		link: function(scope, elem, attrs) {
@@ -97,10 +98,10 @@ var wizard = angular.module('pdWizard', [])
 .controller("WizardController", function($scope, $http, $rootScope, config) {
 	// Tabs for Wizard
 	$scope.tabs = [
-		{title:'Display', template: '/app/wizard/templates/_display.html',},
-		{title:'Survey', template: '/app/wizard/templates/_survey.html'},
-		{title:'Campaign', template: '/app/wizard/templates/_campaign.html'},
-		{title:'Embed Code', template: '/app/wizard/templates/_embedCode.html'}
+		{title:'Add Displays', template: '/app/wizard/templates/_display.html',},
+		{title:'Choose Surveys', template: '/app/wizard/templates/_survey.html'},
+		{title:'Manage Campaigns', template: '/app/wizard/templates/_campaign.html'},
+		{title:'Publish', template: '/app/wizard/templates/_embedCode.html'}
 	];
 	$scope.tabs.activeTab = 0;
 
@@ -141,7 +142,7 @@ var wizard = angular.module('pdWizard', [])
 
 
 	/*** 4) EMBED CODE ***/
-
+	$scope.embedCode = "var jQl={q:[],dq:[],gs:[],ready:function(a){'function'==typeof a&&jQl.q.push(a);return jQl},getScript:function(a,c){jQl.gs.push([a,c])},unq:function(){for(var a=0;a<jQl.q.length;a++)jQl.q[a]();jQl.q=[]},ungs:function(){for(var a=0;a<jQl.gs.length;a++)jQuery.getScript(jQl.gs[a][0],jQl.gs[a][1]);jQl.gs=[]},bId:null,boot:function(a){'undefined'==typeof window.jQuery.fn?jQl.bId||(jQl.bId=setInterval(function(){jQl.boot(a)},25)):(jQl.bId&&clearInterval(jQl.bId),jQl.bId=0,jQl.unqjQdep(),jQl.ungs(),jQuery(jQl.unq()), 'function'==typeof a&&a())},booted:function(){return 0===jQl.bId},loadjQ:function(a,c){setTimeout(function(){var b=document.createElement('script');b.src=a;document.getElementsByTagName('head')[0].appendChild(b)},1);jQl.boot(c)},loadjQdep:function(a){jQl.loadxhr(a,jQl.qdep)},qdep:function(a){a&&('undefined'!==typeof window.jQuery.fn&&!jQl.dq.length?jQl.rs(a):jQl.dq.push(a))},unqjQdep:function(){if('undefined'==typeof window.jQuery.fn)setTimeout(jQl.unqjQdep,50);else{for(var a=0;a<jQl.dq.length;a++)jQl.rs(jQl.dq[a]); jQl.dq=[]}},rs:function(a){var c=document.createElement('script');document.getElementsByTagName('head')[0].appendChild(c);c.text=a},loadxhr:function(a,c){var b;b=jQl.getxo();b.onreadystatechange=function(){4!=b.readyState||200!=b.status||c(b.responseText,a)};try{b.open('GET',a,!0),b.send('')}catch(d){}},getxo:function(){var a=!1;try{a=new XMLHttpRequest}catch(c){for(var b=['MSXML2.XMLHTTP.5.0','MSXML2.XMLHTTP.4.0','MSXML2.XMLHTTP.3.0','MSXML2.XMLHTTP','Microsoft.XMLHTTP'],d=0;d<b.length;++d){try{a= new ActiveXObject(b[d])}catch(e){continue}break}}finally{return a}}};if('undefined'==typeof window.jQuery){var $=jQl.ready,jQuery=$;$.getScript=jQl.getScript}; jQl.loadjQ('//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js') var _paq = _paq || []; (function () {     var u = (('https:' == document.location.protocol) ? 'https://localhost:3000/' : 'http://localhost:3000/'); var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0]; g.type = 'text/javascript'; g.defer = true; g.async = true; g.src = u + 'tracking/survey.js'; s.parentNode.insertBefore(g, s); })();";
 
 
 })
