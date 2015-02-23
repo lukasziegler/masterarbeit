d653fc05d52ab800c0dfea9b3f1973d8f98536d7
Lukas Ziegler <lukas@lukasziegler.com>
2015-02-23 17:09:49 +0100

    PDEmail: added line-by-line approach

--- pdemail/readLog.py
+++ pdemail/readLog.py
@@ -1,17 +1,42 @@
 import csv
 
+
+## READ LINE BY LINE
+
 with open("log/test.txt") as tsv:
-    for column in zip(*[line for line in csv.reader(tsv, dialect="excel-tab")]):
-    	#print columns
-    	print column
+    for line in csv.reader(tsv, dialect="excel-tab"): #You can also use delimiter="\t" rather than giving a dialect.
+    	
+    	## Debug output: print lines
+    	# print line
 
-    	# select correct column
-    	if (column[0] == 'OptionChosen'):
+    	## Check if SendEmail Option was chosen
+    	if line[2] == '2':
+    		print "Send email to:",line[4]
 
-    		# check all entries
-    		for i, option in enumerate(column[1:]):
+    		## TODO: sendEmail(email)
 
-    			# if Option == 2 (via Email)
-    			if option == '2':
-    				#send email
-    				print "SEND EMAIL TO"
+    		## TODO: delete entry or mark as done
+
+
+
+
+
+
+
+## READ COLUMN BY COLUMN
+
+# with open("log/test.txt") as tsv:
+#     for column in zip(*[line for line in csv.reader(tsv, dialect="excel-tab")]):
+#     	#print columns
+#     	print column
+
+#     	# select correct column
+#     	if (column[0] == 'OptionChosen'):
+
+#     		# check all entries
+#     		for i, option in enumerate(column[1:]):
+
+#     			# if Option == 2 (via Email)
+#     			if option == '2':
+#     				#send email
+#     				print "SEND EMAIL TO"
