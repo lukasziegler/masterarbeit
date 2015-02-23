258a87f7f9aac96410521db6a3c39842c2377ebe
Lukas Ziegler <lukas@lukasziegler.com>
2015-02-23 17:04:38 +0100

    PDEmail: approach for reading CSV column-by-column

--- pdemail/readLog.py
+++ pdemail/readLog.py
@@ -2,7 +2,16 @@ import csv
 
 with open("log/test.txt") as tsv:
     for column in zip(*[line for line in csv.reader(tsv, dialect="excel-tab")]):
-    	#print column
+    	#print columns
     	print column
 
-    	#print len(line)
+    	# select correct column
+    	if (column[0] == 'OptionChosen'):
+
+    		# check all entries
+    		for i, option in enumerate(column[1:]):
+
+    			# if Option == 2 (via Email)
+    			if option == '2':
+    				#send email
+    				print "SEND EMAIL TO"
