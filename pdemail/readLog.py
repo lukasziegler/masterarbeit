e5b38a09d0c154b178e0f381ba3223a45cbe5150
lukasziegler <lukas@lukasziegler.com>
2015-03-01 09:47:34 +0100

    PDEmail: added read/write lastLine log capability to know where I left off the last time

--- pdemail/readLog.py
+++ pdemail/readLog.py
@@ -1,57 +1,57 @@
 import csv
 import re
 
-### Check where the last 
-lastExecution = 5
-
-# with open("log/lastLine.txt") as tsv:
-#     for line in csv.reader(tsv, dialect="excel-tab"):
-
-
-
-### READ LINE BY LINE
-with open("log/Survey.txt") as tsv:
-    for line in csv.reader(tsv, dialect="excel-tab"): #You can also use delimiter="\t" rather than giving a dialect.
-    	
-    	### File Overview
-    	## 1. ID
-    	## 2. BonusCode
-    	## 3. Date
-    	## 4. Email
-    	## 5. Choice
-    	## 6. FinalStage
-
-    	### Debug output: print lines
-    	print line
-
-    	## Check if Email is Valid
-    	if re.match(r"[^@]+@[^@]+\.[^@]+", line[3]):
-    		print "Send email to:",line[3]
-
-    		## TODO: sendEmail(email)
-
-    		## TODO: delete entry or mark as done
+### Helper functions
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 
 
+lastLine = -1
+
+### Check where the Scheduler last left off
+lineLog = open('log/lastLine.txt', 'r')
+lastLine = lineLog.read()
+print "lastLine:",lastLine
 
 
+### READ SURVEY LOG (line by line)
+i = 0
+surveyLog = open('log/Survey.txt')
+for line in csv.reader(surveyLog, dialect="excel-tab"): #You can also use delimiter="\t" rather than giving a dialect.
+	# print line
+
+	### LogFile Overview ###
+	## 1. ID		# 2. BonusCode
+	## 3. Date		# 4. Email
+	## 5. Choice	# 6. FinalStage
+
+	# if i >= lastLine:
+	if i >= int(lastLine):
+		print "new line",i
+		
+	## Check if Email is Valid
+	# if re.match(r"[^@]+@[^@]+\.[^@]+", line[3]):
+	# 	print "Send email to:",line[3]
+
+		## TODO: sendEmail(email)
+
+		## TODO: delete entry or mark as done
+		# print i
+
+	i += 1
+
+surveyLog.close()
 
 
-## READ COLUMN BY COLUMN
+### UPDATE LAST LINE-LOG
+lastLine = i
+print "new lastLine:",lastLine
 
-# with open("log/test.txt") as tsv:
-#     for column in zip(*[line for line in csv.reader(tsv, dialect="excel-tab")]):
-#     	#print columns
-#     	print column
-
-#     	# select correct column
-#     	if (column[0] == 'OptionChosen'):
-
-#     		# check all entries
-#     		for i, option in enumerate(column[1:]):
-
-#     			# if Option == 2 (via Email)
-#     			if option == '2':
-#     				#send email
-#     				print "SEND EMAIL TO"
+lineLog = open('log/lastLine.txt', "wb")
+lineLog.write(str(lastLine))
+lineLog.close()
