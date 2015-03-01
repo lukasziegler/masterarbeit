3316e0ddc04e5eef7ae928f008acb9d9eb13a951
lukasziegler <lukas@lukasziegler.com>
2015-03-01 09:16:24 +0100

    PDEmail: added regExp (email) support

--- pdemail/readLog.py
+++ pdemail/readLog.py
@@ -1,17 +1,32 @@
 import csv
+import re
+
+### Check where the last 
+lastExecution = 5
+
+# with open("log/lastLine.txt") as tsv:
+#     for line in csv.reader(tsv, dialect="excel-tab"):
 
 
-## READ LINE BY LINE
 
-with open("log/test.txt") as tsv:
+### READ LINE BY LINE
+with open("log/Survey.txt") as tsv:
     for line in csv.reader(tsv, dialect="excel-tab"): #You can also use delimiter="\t" rather than giving a dialect.
     	
-    	## Debug output: print lines
-    	# print line
+    	### File Overview
+    	## 1. ID
+    	## 2. BonusCode
+    	## 3. Date
+    	## 4. Email
+    	## 5. Choice
+    	## 6. FinalStage
 
-    	## Check if SendEmail Option was chosen
-    	if line[2] == '2':
-    		print "Send email to:",line[4]
+    	### Debug output: print lines
+    	print line
+
+    	## Check if Email is Valid
+    	if re.match(r"[^@]+@[^@]+\.[^@]+", line[3]):
+    		print "Send email to:",line[3]
 
     		## TODO: sendEmail(email)
 
