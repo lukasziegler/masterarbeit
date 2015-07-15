75bde423b8234a79f63ac7c0c4c3c0f310d00c09
lukasziegler <lukas@lukasziegler.com>
2015-07-16 00:47:08 +0200

    audio fixed

--- pdsurvey/pdclient/lib/audio-recording.js
+++ pdsurvey/pdclient/lib/audio-recording.js
@@ -28,7 +28,7 @@
         var textarea = document.querySelector('#results');
         for (var i = e.resultIndex; i < e.results.length; ++i) {
             if (e.results[i].isFinal) {
-                $('#results').val(e.results[i][0].transcript);
+                textarea.value += e.results[i][0].transcript;
             }
         }
     }
